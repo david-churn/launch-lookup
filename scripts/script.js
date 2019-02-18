608a90aa995a404ff271063f940faf9ec08f1e66
David Churn <dchurn@me.com>
2019-02-18 11:29:07 -0600

    Cleaned up JavaScript testing statements and sharpened comments. Did final polish on display for initial coding v1.0 achieved.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 'use strict';
-// Screen to display rocket launchs using data from the Launch Library.
+// Screen to display rocket launch data from the Launch Library.
 //   https://launchlibrary.net
 // 2/12/2019 David Churn created
 
@@ -16,16 +16,15 @@ const msnTableObj = document.getElementById("mtable");
 const msnDetailHdrObj = document.getElementById("l-header");
 const msnDetailObj = document.getElementById("mdetail");
 const messageObj = document.getElementById("message");
+// the base URL for all requests.
 const llUrl = "https://launchlibrary.net/1.4/";
 
-// look up mission status values to populate drop down list and display array.
+// Look up mission status values to populate a drop down list and a display array.
 let statusArr = [];
-// fetch a list from launchlibrary
 let statusUrl = llUrl + 'launchstatus';
 fetch(statusUrl)
   .then( (response) => response.json() )
   .then(function(statusObj) {
-    console.log('statusObj=', statusObj);
     if ( statusObj.count > 0 ) {
       statusArr = statusObj.types;
       for (let index in statusObj.types) {
@@ -38,15 +37,14 @@ fetch(statusUrl)
     }
   });
 
-// look up the launch service providers for a display array.
+// Look up the launch service providers for a display array.
+//   Note: In the URL "limit=-1" means "all available"
+//                    "mode=S" supposedly means "less information" but seems to be ignored.
 let lspArr = [];
-// fetch a list from launchlibrary
-//   limit=-1 means "all available"
 let lspUrl = llUrl + 'agency?islsp=1&mode=S&limit=-1';
 fetch(lspUrl)
   .then( (response) => response.json() )
   .then(function(lspObj) {
-    console.log('lspObj=', lspObj);
     if ( lspObj.count > 0 ) {
       lspArr = lspObj.agencies;
       // for (let index in lspObj.types) {
@@ -59,14 +57,13 @@ fetch(lspUrl)
     }
   });
 
-// Everything requested and waiting for a human action.
-  console.log('Project Launch ready!');
-
+// Display the mission list when the user clicks the search button
+//   using the parameters they entered (or ignored).
 searchBtnObj.addEventListener('click', function() {
   clearList();
   let searchUrl = llUrl + "launch?";
 
-  // Check the input and use valid values
+  // Check the input and add the valid values to the request
   if (msnNameObj.value != '') {
     searchUrl += ('name=' + msnNameObj.value + '&')
   }
@@ -86,13 +83,9 @@ searchBtnObj.addEventListener('click', function() {
     searchUrl += 'limit=10';
   }
 
-  // What was just built?!?
-  console.log(`searchUrl=${searchUrl}`);
-  // fetch a list from launchlibrary
   fetch(searchUrl)
     .then( (response) => response.json() )
     .then(function(launchObj) {
-      console.log('launchObj=', launchObj);
       if ( launchObj.count > 0 ) {
         addTableHeading();
         for (let item in launchObj.launches) {
@@ -107,6 +100,7 @@ searchBtnObj.addEventListener('click', function() {
     });
 });
 
+// Reveal the subsection heading and build the table headings.
 function addTableHeading() {
 // make subheading title visible
   msnTableHdrObj.setAttribute("class","");
@@ -131,7 +125,7 @@ function addTableHeading() {
   launchID.outerHTML = "<th class='hidden'>ID</th>";
 }
 
-// build the mission list creating event listeners as you go.
+// Build the launch list creating event listeners as you go.
 function postLaunch(listObj) {
   let row = msnTableObj.insertRow();
   msnTableObj.appendChild(row);
@@ -150,12 +144,12 @@ function postLaunch(listObj) {
   lspAbbrev.innerHTML = lspArr.find(obj => obj.id == listObj.lsp).abbrev;
   lspAbbrev.setAttribute("class","center");
 
+// This is an acceptable use of the <br>.
+//   The replace takes out all the bars '|' and put a line break in.
   let launchNm = row.insertCell();
-  launchNm.innerHTML = listObj.name;
-  // split the line
-  // .replace(/\|/g,"<br>");
+  launchNm.innerHTML = listObj.name.replace(/\|/g,"<br>");
 
-// >> This must be the last element in the row. <<
+// >> Important! This must be the last element in the row for the Launch Detail. <<
   let launchID = row.insertCell();
   launchID.setAttribute("class","hidden");
   launchID.innerHTML = listObj.id;
@@ -172,7 +166,7 @@ clearBtnObj.addEventListener('click', function() {
   clearList();
 });
 
-// clean up mission list (+event listeners) and mission detail areas
+// clean up launch list (+event listeners) and launch detail areas
 function clearList() {
   clearDetail();
   while (msnTableObj.hasChildNodes()) {
@@ -182,19 +176,15 @@ function clearList() {
   msnTableHdrObj.setAttribute("class","hidden");
 }
 
-// fetch the detail from Launch Library and display the details
+// Fetch the detail for one launch and display them.
 function getDetail(clickObj) {
-  console.log('getDetail=', clickObj);
-  // clear detail area
   clearDetail();
-  // build mission as heading and appropriate details
+  // build mission as heading and appropriate details from the hidden ID in the launch list.
   let detailUrl = llUrl + "launch?mode=verbose&id=" + clickObj.srcElement.parentNode.lastChild.textContent;
-  // What was just built?!?
-  console.log(`detailUrl=${detailUrl}`);
+
   fetch(detailUrl)
     .then( (response) => response.json() )
     .then(function(launchObj) {
-      console.log('launchObj=', launchObj);
       if ( launchObj.count > 0 ) {
         for (let item in launchObj.launches) {
           postDetail(launchObj.launches[item])
@@ -207,31 +197,32 @@ function getDetail(clickObj) {
     })
 }
 
+// Display some of the details found.
 function postDetail(detailObj) {
   msnDetailHdrObj.setAttribute("class","");
 
   let launchNm = document.createElement('h2');
-  launchNm.innerHTML = detailObj.name;
+  launchNm.innerHTML = detailObj.name.replace(/\|/g,"<br>");
   msnDetailObj.appendChild(launchNm);
 
   let lspNm = document.createElement('p');
   lspNm.innerHTML = `by ${detailObj.lsp.name}`;
   msnDetailObj.appendChild(lspNm);
 
-  let launchDt = document.createElement('p');
-  launchDt.innerHTML = `launch: ${detailObj.net}`;
-  msnDetailObj.appendChild(launchDt);
-
   let msnStat = document.createElement('p');
   msnStat.innerHTML = `status: ${statusArr.find(obj => obj.id === detailObj.status).description}`;
   msnDetailObj.appendChild(msnStat);
 
+  let launchDt = document.createElement('p');
+  launchDt.innerHTML = `launch date: ${detailObj.net}`;
+  msnDetailObj.appendChild(launchDt);
+
   let whereNm = document.createElement('p');
-  whereNm.innerHTML = `from: ${detailObj.location.name}`;
+  whereNm.innerHTML = `launch site: ${detailObj.location.name}`;
   msnDetailObj.appendChild(whereNm);
 
   let rocketNm = document.createElement('p');
-  rocketNm.innerHTML = `on: ${detailObj.rocket.name}`;
+  rocketNm.innerHTML = `rocket: ${detailObj.rocket.name}`;
   msnDetailObj.appendChild(rocketNm);
 
   if (detailObj.missions.length > 0) {
